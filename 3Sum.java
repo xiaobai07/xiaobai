2cbc2c4a077ac0906b1567e08adc5581468d5b42
xiaobai07 <chasedreamby@gmail.com>
2013-02-17 14:58:21 -0800

    Create 3Sum.java

---
+++ 3Sum.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        if(num.length<3) return res;
+        
+        Arrays.sort(num);
+        for(int i=0;i<num.length-2;i++){
+            if(i==0 || num[i]>num[i-1]){ //avoid duplicate solutions   
+                int j=i+1, 
+                    k=num.length-1;
+         
+                while(j<k){ 
+                    if(num[j]+num[k]==-num[i]){
+                        ArrayList<Integer> temp = new ArrayList<Integer>();
+                        temp.add(num[i]);
+                        temp.add(num[j]);
+                        temp.add(num[k]);
+                        res.add(temp);
+                        k--;
+                        j++;
+                        while(k>j && num[k]==num[k+1]) k--;//avoid duplicate solutions 
+
+                        while(j<k && num[j]==num[j-1]) j++;//avoid duplicate solutions 
+
+                    }else if(num[j]+num[k]>-num[i]){
+                        k--;
+                    }else{
+                        j++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
