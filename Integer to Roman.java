cb8f4a6be1f23ac5e4de7fc96b5d77cea21784fa
xiaobai07 <chasedreamby@gmail.com>
2013-02-24 19:14:08 -0500

    Create Integer to Roman.java

---
+++ Integer to Roman.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public String intToRoman(int num) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int[] nu={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] ro={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int i=0;
+        String rome="";
+        while(num>0)
+        {
+            while(num>=nu[i]){
+                rome+=ro[i];
+                num-=nu[i];
+            }
+            i++;
+        }
+        return rome;
+    }
+}
