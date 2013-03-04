58c7753b1e8b0d20d215ab59a6b21ef0556b8d6d
xiaobai07 <chasedreamby@gmail.com>
2013-03-04 16:06:38 -0500

    Update Validate Binary Search Tree.java

--- Validate Binary Search Tree.java
+++ Validate Binary Search Tree.java
@@ -14,3 +14,40 @@ public class Solution {
         else return ifvalid(root.left,root.val,min)&&ifvalid(root.right,max,root.val);
     }
 }
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ArrayList<Integer> value=new ArrayList<Integer>();
+        TreeNode cur=root;
+        Stack<TreeNode> res=new Stack<TreeNode>();
+        if(root==null) return true;
+        while(!res.isEmpty()||cur!=null)
+        {
+            if(cur!=null){
+                res.push(cur);
+                cur=cur.left;
+            }
+            else{
+                 cur=res.pop();
+                 value.add(cur.val);
+                 cur=cur.right;
+            }
+        }
+        for(int i=0;i<value.size()-1;i++){
+            if(value.get(i+1)<=value.get(i)) return false;
+        }
+        return true;
+        
+    }
+}
