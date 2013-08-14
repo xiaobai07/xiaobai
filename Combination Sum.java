0c6df1e6ca9b09bcf209421159aab9c971a03a18
xiaobai07 <chasedreamby@gmail.com>
2013-08-14 16:12:14 -0700

    Update Combination Sum.java

--- Combination Sum.java
+++ Combination Sum.java
@@ -1,30 +1,22 @@
-public class Solution {
-    public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        Arrays.sort(candidates);
-        ArrayList<ArrayList<Integer>> re = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> li = new ArrayList<Integer>();
-        getlist(candidates,target,li,re,0);
-        
-        return re;
-        
-    } 
-    
-    public void getlist(int[] candidates, int target, ArrayList<Integer> li,ArrayList<ArrayList<Integer>> reli,int start){
-        if(target == 0){
-            ArrayList<Integer> t = new ArrayList<Integer>(li);
-            if(!reli.contains(t))
-                reli.add(t);
-            return;
-        }
-        
-        for(int i=start; i<candidates.length; i++){
-            if(target-candidates[i]>=0){
-                li.add(candidates[i]);
-                getlist(candidates,target-candidates[i],li,reli,i);
-                li.remove(li.size()-1);                
-            }
-        }
-    }
-}
+public class Solution {  
+    public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {  
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();  
+        ArrayList<Integer> tmp = new ArrayList<Integer>();  
+        Arrays.sort(candidates);  
+        dfs(res,tmp,candidates,target,0);  
+        return res;  
+    }  
+      
+    public void dfs(ArrayList<ArrayList<Integer>> res, ArrayList<Integer> tmp, int[] cand, int target, int pos){  
+        if(target<0) return;  
+        if(target==0){  
+            res.add(new ArrayList<Integer>(tmp));  
+            return;  
+        }  
+        for(int i=pos; i<cand.length; i++){  
+            tmp.add(cand[i]);  
+            dfs(res,tmp,cand,target-cand[i],i);  
+            tmp.remove(tmp.size()-1);  
+        }  
+    }  
+}  
