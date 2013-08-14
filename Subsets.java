0e917c2ba96d3a83daa60ffae1513cc156914577
xiaobai07 <chasedreamby@gmail.com>
2013-08-14 15:54:11 -0700

    Update Subsets.java

--- Subsets.java
+++ Subsets.java
@@ -1,30 +1,19 @@
-public class Solution {
-    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int n=S.length;
-        ArrayList<Integer> col = new ArrayList<Integer>();
-        ArrayList<ArrayList<Integer>> re = new ArrayList<ArrayList<Integer>>();
-        re.add(col);
-        Arrays.sort(S);
-        for(int i=1;i<=n;i++)
-            getlist(S,i,col,re,0);    
-        return re;
-    }
-    
-    public void getlist(int[] s, int k, ArrayList<Integer> col, ArrayList<ArrayList<Integer>> re, int start){
-        if(col.size()==k){
-            ArrayList<Integer> t = new ArrayList<Integer>(col);
-            re.add(t);
-            return;
-        }
-        
-        for(int i=start; i<s.length; i++){
-            col.add(s[i]);
-            getlist(s,k,col,re,i+1);
-            col.remove(col.size()-1);
-        }
-        
-    }
-    
-}
+public class Solution {  
+    public ArrayList<ArrayList<Integer>> subsets(int[] S) {  
+       ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();  
+       ArrayList<Integer> tmp = new ArrayList<Integer>();  
+       Arrays.sort(S);  
+       res.add(tmp);  
+       dfs(res,tmp,S,0);  
+       return res;  
+    }  
+      
+    public void dfs(ArrayList<ArrayList<Integer>> res, ArrayList<Integer> tmp, int[] S, int pos){  
+        for(int i=pos; i<=S.length-1;i++){  
+            tmp.add(S[i]);  
+            res.add(new ArrayList<Integer>(tmp));  
+            dfs(res,tmp,S,i+1);  
+            tmp.remove(tmp.size()-1);  
+        }  
+    }  
+}  
