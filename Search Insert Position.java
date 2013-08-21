558ab4d28621df421156559f47b2b5f88aaead60
xiaobai07 <chasedreamby@gmail.com>
2013-08-21 09:55:44 -0700

    Update Search Insert Position.java

--- Search Insert Position.java
+++ Search Insert Position.java
@@ -1,13 +1,13 @@
-public class Solution {
-    public int searchInsert(int[] A, int target) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-
-        for(int i=0;i<A.length;i++)
-        {
-            if(A[i]==target) return i;
-            if(A[i]>target)  return i;
-        }
-        return A.length;
-    }
-}
+public class Solution {  
+    public int searchInsert(int[] A, int target) {  
+        int start = 0;  
+        int end = A.length - 1;  
+        while(start<=end){  
+            int mid = start + (end - start)/2;  
+            if (A[mid] == target) return mid;  
+            else if (A[mid] > target) end = mid - 1;  
+            else start = mid + 1;  
+        }  
+        return start;  
+    }  
+} 
