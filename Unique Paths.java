ba67fe031940a353e11bee8c56b1b860b95296a4
xiaobai07 <chasedreamby@gmail.com>
2013-08-23 00:33:28 -0700

    Update Unique Paths.java

--- Unique Paths.java
+++ Unique Paths.java
@@ -2,22 +2,19 @@ public class Solution {
     public int uniquePaths(int m, int n) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        if(m==0 || n==0) return 0;
-        if(m==1 || n==1) return 1;        
-        int[][] path = new int[m][n];        
-        for(int i=n-1;i>=0;i--)
-        {
-            path[m-1][i]=1;
+        if(m ==0 || n ==0) return 0;
+        int[][] matrix = new int[m][n];
+        for(int i = 0;i < m; i++){
+            matrix[i][0] = 1;
+            for(int j = 1;j < n; j++){
+                if( i == 0){
+                    matrix[i][j] = 1;
+                }
+                else{
+                    matrix[i][j] = matrix[i-1][j] + matrix[i][j-1];
+                }
+            }
         }
-        for(int i=m-1;i>=0;i--)
-        {
-            path[i][n-1]=1;
-        }
-        for(int i=m-2;i>=0;i--)
-           for(int j=n-2;j>=0;j--)
-           {
-              path[i][j]=path[i+1][j]+path[i][j+1];
-           }   
-        return path[0][0];   
+        return matrix[m-1][n-1];
     }
 }
