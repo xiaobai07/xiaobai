1c3bff05ea81d88c1b453ec8ad7a746836cf1c79
xiaobai07 <chasedreamby@gmail.com>
2013-09-16 11:33:01 -0700

    Update Integer to Roman.java

--- Integer to Roman.java
+++ Integer to Roman.java
@@ -5,15 +5,15 @@ public class Solution {
         int[] nu={1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String[] ro={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         int i=0;
-        String rome="";
+        StringBuilder rome = new StringBuilder();
         while(num>0)
         {
             while(num>=nu[i]){
-                rome+=ro[i];
+                rome.append(ro[i]);
                 num-=nu[i];
             }
             i++;
         }
-        return rome;
+        return rome.toString();
     }
 }
