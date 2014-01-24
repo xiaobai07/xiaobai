f8d6a5eca1f61695faee5e217322cc04da8ec191
xiaobai07 <chasedreamby@gmail.com>
2014-01-23 22:52:15 -0800

    Update Permutations.java

--- Permutations.java
+++ Permutations.java
@@ -28,3 +28,54 @@ public class Solution {
 		}
 	}
 }
+
+public class Solution {
+    public ArrayList<ArrayList<Integer>> permute(int[] num) {
+        ArrayList<Integer> hs = new ArrayList<Integer>();
+        HashSet<Integer> remain = new HashSet<Integer>();
+        for(int i: num)
+            remain.add(i);
+        return perHelper(hs,remain);
+    }
+    public ArrayList<ArrayList<Integer>> perHelper(ArrayList<Integer> hs,HashSet<Integer> remain){
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if(remain.size() == 0){
+            ArrayList<ArrayList<Integer>> rs = new ArrayList<ArrayList<Integer>>();
+            rs.add(hs);
+            return rs;
+            }
+        else
+            for(Integer in :remain){
+                ArrayList<Integer> tempA = new ArrayList<Integer>(hs);
+                tempA.add(in);
+                HashSet<Integer> temp  = new HashSet<Integer>(remain);
+                temp.remove(in);
+                result.addAll(perHelper(tempA,temp));
+            }
+        return result;
+    }
+}
+
+public class Permutations { public ArrayList<arraylist<integer>> permute(int[] num) {
+
+    ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+    ArrayList<Integer> a = new ArrayList<Integer>();
+    a.add(num[0]);
+    list.add(a);
+    for(int i = 1; i < num.length; i++) {
+        list = insert(list, num[i]);
+    }
+    return list;
+}
+
+public ArrayList<ArrayList<Integer>> insert(ArrayList<ArrayList<Integer>> list, int num) {
+    ArrayList<ArrayList<Integer>> resultList = new ArrayList<ArrayList<Integer>>();
+    for(int i = 0; i < list.size(); i++) {
+        for (int j = 0; j < list.get(i).size() + 1; j++) {
+            ArrayList<Integer> temp = new ArrayList<Integer>(list.get(i));
+            temp.add(j, num);
+            resultList.add(temp);
+        }
+    }
+    return resultList;
+}
